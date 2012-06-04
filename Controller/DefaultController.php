130d65838811edf2323f8a82fa6e6ee85c7228e8
Rosterbot User <rosterbot@rosterbot.com>
2012-06-03 18:58:26 -0700

    initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Striide\InventoryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('StriideInventoryBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ StriideInventoryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Striide\InventoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StriideInventoryBundle extends Bundle
+{
+}
