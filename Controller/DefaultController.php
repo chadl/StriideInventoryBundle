4b91b832724e8015748f4dfc7bcd3745e502772a
Chad Lindstrom <chad.lindstrom@gmail.com>
2012-06-03 19:34:55 -0700

    reformatting

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace Striide\InventoryBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
-    {
-        return $this->render('StriideInventoryBundle:Default:index.html.twig', array('name' => $name));
-    }
+  public function indexAction($name)
+  {
+    return $this->render('StriideInventoryBundle:Default:index.html.twig', array(
+      'name' => $name
+    ));
+  }
 }
--- StriideInventoryBundle.php
+++ StriideInventoryBundle.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Striide\InventoryBundle;
-
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class StriideInventoryBundle extends Bundle
